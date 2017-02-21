1b4ec040f789d05cfb25403d9ba0c593e42283b7
jerryjee <qsbaq@163.com>
2017-02-21 16:44:01 +0800

    修改示例文件

--- Application/User/Conf/config.sample.php
+++ Application/User/Conf/config.sample.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'LmCx:2E+z[!kD"1N>U~3cB{f9WSqZt]Tl/V&PM|0'); //加密KEY
-define('UC_DB_DSN', 'mysqli://btlet:jjjjjj@v.laoji.org:3306/btlet'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://btlet:laoji.org@btlet:3306/btlet'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'laoji_'); // 数据表前缀，使用Model方式调用API必须配置此项
