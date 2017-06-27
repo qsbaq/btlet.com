d22a7125c4e77cbc78ecb89ade36788372cded3d
jerryjee <qsbaq@163.com>
2017-06-27 13:59:34 +0800

    兼容sphinx

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -42,7 +42,9 @@ class IndexController extends HomeController {
         }
       
         if( C('USE_SPHINX') ){
-            import('Vendor.Sphinx');
+            if( ! class_exists('SphinxClient',false)){
+                import('Vendor.Sphinx');
+            }
             $nowPage = I('p');//当前页
             import('ORG.Util.Page');
             $listRows = C('LIST_ROWS') > 0 ? C('LIST_ROWS') : 10;
